bb6abea8cc821df0fedb02849c0662c9f409317a
고경민 <gkm2164@gmail.com>
2017-10-05 14:57:37 +0900

    Code refactoring
    packed functions with object to separate problems

--- src/main/scala/co/gyeongmin/problem/Problems.scala
+++ src/main/scala/co/gyeongmin/problem/Problems.scala
@@ -5,6 +5,7 @@ import scala.language.postfixOps
 import scala.util.Random
 
 object Problems {
+
   // P01
   object P01 {
     @tailrec
@@ -56,188 +57,251 @@ object Problems {
     }
   }
 
-  // P05
-  def reverse[A](l: List[A]): List[A] = {
-    @tailrec
-    def reverse(current: List[A], result: List[A]): List[A] = {
-      current match {
-        case Nil => result
-        case h :: t => reverse(t, h :: result)
-      }
-    }
-
-    reverse(l, Nil)
-  }
-
-  // P06
-  def isPalindrome[A](l: List[A]) = l == reverse(l)
-
-  // P07
-  def flatten(l: List[Any]) = {
-    @tailrec
-    def flatten(l: List[Any], result: List[Any]): List[Any] = {
-      l match {
-        case Nil => result
-        case (h: List[_]) :: t => flatten(h ::: t, result)
-        case h :: t => flatten(t, result :+ h)
-      }
-    }
-    flatten(l, Nil)
-  }
-
-  def compress[A](l: List[A]) = {
-    @tailrec
-    def compress(l: List[A], result: List[A]): List[A] =
-      if (l.isEmpty) result
-      else {
-        val (_, tail) = l span { _ == l.head }
-
-        compress(tail, result :+ l.head)
+  object P05 {
+    // P05
+    def reverse[A](l: List[A]): List[A] = {
+      @tailrec
+      def reverse(current: List[A], result: List[A]): List[A] = {
+        current match {
+          case Nil => result
+          case h :: t => reverse(t, h :: result)
+        }
       }
 
-    compress(l, Nil)
+      reverse(l, Nil)
+    }
   }
 
-  def pack(l: List[Any]) = {
-    @tailrec
-    def pack(l: List[Any], result: List[Any]): List[Any] =
-      if (l.isEmpty) result
-      else {
-        val (s, rest) = l span { _ == l.head }
-        pack(rest, result :+ s)
+  object P06 {
+
+    import P05._
+
+    // P06
+    def isPalindrome[A](l: List[A]) = l == reverse(l)
+  }
+
+  object P07 {
+    // P07
+    def flatten(l: List[Any]) = {
+      @tailrec
+      def flatten(l: List[Any], result: List[Any]): List[Any] = {
+        l match {
+          case Nil => result
+          case (h: List[_]) :: t => flatten(h ::: t, result)
+          case h :: t => flatten(t, result :+ h)
+        }
       }
-    pack(l, Nil)
+
+      flatten(l, Nil)
+    }
   }
 
-  def encode[A](list: List[A]) = {
-    @tailrec
-    def encode(l: List[A], result: List[(Int, A)]): List[(Int, A)] = {
-      if (l.isEmpty) result
-      else {
-        val (s, rest) = l span { _ == l.head }
-        encode(rest, (s.length, l.head) :: result)
+  object P08 {
+    def compress[A](l: List[A]) = {
+      @tailrec
+      def compress(l: List[A], result: List[A]): List[A] =
+        if (l.isEmpty) result
+        else {
+          val (_, tail) = l span {
+            _ == l.head
+          }
+
+          compress(tail, result :+ l.head)
+        }
+
+      compress(l, Nil)
+    }
+  }
+
+  object P09 {
+    def pack(l: List[Any]) = {
+      @tailrec
+      def pack(l: List[Any], result: List[Any]): List[Any] =
+        if (l.isEmpty) result
+        else {
+          val (s, rest) = l span {
+            _ == l.head
+          }
+          pack(rest, result :+ s)
+        }
+
+      pack(l, Nil)
+    }
+  }
+
+  object P10 {
+    def encode[A](list: List[A]) = {
+      @tailrec
+      def encode(l: List[A], result: List[(Int, A)]): List[(Int, A)] = {
+        if (l.isEmpty) result
+        else {
+          val (s, rest) = l span {
+            _ == l.head
+          }
+          encode(rest, result :+ (s.length, l.head))
+        }
       }
-    }
 
-    reverse(encode(list, List()))
+      encode(list, List())
+    }
   }
 
-  def encodeModified[A](list: List[A]) = {
-    @tailrec
-    def encodeModified(l: List[A], result: List[Any]): List[Any] = {
-      if (l.isEmpty) result
-      else {
-        val (s, rest) = l span { _ == l.head }
-        if (s.length > 1)
-          encodeModified(rest, (s.length, l.head) :: result)
-        else
-          encodeModified(rest, l.head :: result)
+  object P11 {
+    def encodeModified[A](list: List[A]) = {
+      @tailrec
+      def encodeModified(l: List[A], result: List[Any]): List[Any] = {
+        if (l.isEmpty) result
+        else {
+          val (s, rest) = l span {
+            _ == l.head
+          }
+          if (s.length > 1)
+            encodeModified(rest, result :+ (s.length, l.head))
+          else
+            encodeModified(rest, result :+ l.head)
+        }
       }
+
+      encodeModified(list, List())
     }
-
-    reverse(encodeModified(list, List()))
   }
 
-  def decode[A](list: List[(Int, A)]): List[A] = {
-    list.flatMap(item => List.fill(item._1)(item._2))
+  object P12 {
+    def decode[A](list: List[(Int, A)]): List[A] = {
+      list.flatMap(item => List.fill(item._1)(item._2))
+    }
   }
 
-  def encodeDirect[A](list: List[A]): List[(Int, A)] = {
-    @tailrec
-    def encodeDirect(remain: List[A], current: (Int, A), result: List[(Int, A)]): List[(Int, A)] = {
-      remain match {
-        case Nil => current :: result
-        case head :: tail if head != current._2 =>
-          encodeDirect(tail, (1, head), current :: result)
-        case head :: tail if head == current._2 =>
-          encodeDirect(tail, (current._1 + 1, head), result)
+
+  object P13 {
+    def encodeDirect[A](list: List[A]): List[(Int, A)] = {
+      @tailrec
+      def encodeDirect(remain: List[A], current: (Int, A), result: List[(Int, A)]): List[(Int, A)] = {
+        remain match {
+          case Nil => result :+ current
+          case head :: tail if head != current._2 =>
+            encodeDirect(tail, (1, head), result :+ current)
+          case head :: tail if head == current._2 =>
+            encodeDirect(tail, (current._1 + 1, head), result)
+        }
       }
+
+      encodeDirect(list.tail, (1, list.head), List())
     }
-
-    reverse(encodeDirect(list.tail, (1, list.head), List()))
   }
 
-  def duplicate[A](list: List[A]): List[A] = {
-    def duplicate(list: List[A], remain: List[A]): List[A] = {
-      if(list.isEmpty) remain
-      else duplicate(list.tail, list.head :: list.head :: remain)
-    }
-
-    duplicate(list, List()).reverse
-  }
-
-  def duplicateN[A](repeat: Int, list: List[A]): List[A] = {
-    def duplicate(list: List[A], remain: List[A]): List[A] = {
-      if(list.isEmpty) remain
-      else duplicate(list.tail, List.fill(repeat)(list.head) ::: remain)
-    }
-
-    duplicate(list, List()).reverse
-  }
-
-  def drop[A](n: Int, list: List[A]): List[A] = {
-    @tailrec
-    def drop(rem: Int, list: List[A], remain: List[A]): List[A] = {
-      if (list.isEmpty) remain
-      else if (rem <= 1) drop(n, list.tail, remain)
-      else drop(rem - 1, list.tail, list.head :: remain)
-    }
-
-    drop(n, list, List()).reverse
-  }
-
-  def split[A](n: Int, list: List[A]) = {
-    @tailrec
-    def split(n: Int, list: List[A], a: List[A]): (List[A], List[A]) = {
-      if (list.isEmpty) (a, Nil)
-      else if (n < 1) (a, list)
-      else split(n - 1, list.tail, a ::: List(list.head))
-    }
-
-    split(n, list, List())
-  }
-
-  def slice[A](n: Int, m: Int, list: List[A]) = {
-    split(m - n, split(n, list)._2)._1
-  }
-
-  @tailrec
-  def rotate[A](n: Int, list: List[A]): List[A] = {
-    val nBounded = if (list.isEmpty) 0 else n % list.length
-    if (nBounded < 0) rotate(nBounded + list.length, list)
-    else (list drop nBounded) ::: (list take nBounded)
-  }
-
-  def removeAt[A](n: Int, list: List[A]): (List[A], A) =
-    ((list take n) ::: (list drop (n + 1)), list(n))
-
-  def insertAt[A](item: A, n: Int, list: List[A]): List[A] =
-    (list take n) ::: (item :: (list drop n))
-
-  def range(n: Int, m: Int): List[Int] = {
-    def range(end: Int, list: List[Int]): List[Int] = {
-      if (n > end) list
-      else range(end - 1, end :: list)
-    }
-
-    range(m, Nil)
-  }
-
-  def randomSelect[A](n: Int, list: List[A]): List[A] = {
-    def randomSelect(n: Int, list: List[A], result: List[A]): List[A] = {
-      if (n == 0) result
-      else {
-        val (remain, item) = removeAt(new Random(System.currentTimeMillis()).nextInt(n), list)
-        randomSelect(n - 1, remain, item :: result)
+  object P14 {
+    def duplicate[A](list: List[A]): List[A] = {
+      @tailrec
+      def duplicate(list: List[A], remain: List[A]): List[A] = {
+        if (list.isEmpty) remain
+        else duplicate(list.tail, remain :+ list.head :+ list.head)
       }
-    }
 
-    randomSelect(n, list, Nil)
+      duplicate(list, List())
+    }
   }
 
-  def lotto(n: Int, max: Int) = randomSelect(n, range(1, max))
+  object P15 {
+    def duplicateN[A](repeat: Int, list: List[A]): List[A] = {
+      def duplicate(list: List[A], remain: List[A]): List[A] = {
+        if (list.isEmpty) remain
+        else duplicate(list.tail, List.fill(repeat)(list.head) ::: remain)
+      }
 
-  def randomPermute[A](items: List[A]) = randomSelect(items.length, items)
+      duplicate(list, List()).reverse
+    }
+  }
+
+  object P16 {
+    def drop[A](n: Int, list: List[A]): List[A] = {
+      @tailrec
+      def drop(rem: Int, list: List[A], remain: List[A]): List[A] = {
+        if (list.isEmpty) remain
+        else if (rem <= 1) drop(n, list.tail, remain)
+        else drop(rem - 1, list.tail, list.head :: remain)
+      }
+
+      drop(n, list, List()).reverse
+    }
+  }
+
+  object P17 {
+    def split[A](n: Int, list: List[A]) = {
+      @tailrec
+      def split(n: Int, list: List[A], a: List[A]): (List[A], List[A]) = {
+        if (list.isEmpty) (a, Nil)
+        else if (n < 1) (a, list)
+        else split(n - 1, list.tail, a :+ list.head)
+      }
+
+      split(n, list, List())
+    }
+  }
+
+  object P18 {
+    import P17._
+    def slice[A](n: Int, m: Int, list: List[A]) = {
+      split(m - n, split(n, list)._2)._1
+    }
+  }
+
+  object P19 {
+    @tailrec
+    def rotate[A](n: Int, list: List[A]): List[A] = {
+      val nBounded = if (list.isEmpty) 0 else n % list.length
+      if (nBounded < 0) rotate(nBounded + list.length, list)
+      else (list drop nBounded) ::: (list take nBounded)
+    }
+  }
+
+  object P20 {
+    def removeAt[A](n: Int, list: List[A]): (List[A], A) =
+      ((list take n) ::: (list drop (n + 1)), list(n))
+  }
+
+  object P21 {
+    def insertAt[A](item: A, n: Int, list: List[A]): List[A] =
+      (list take n) ::: (item :: (list drop n))
+  }
+
+  object P22 {
+    def range(n: Int, m: Int): List[Int] = {
+      def range(end: Int, list: List[Int]): List[Int] = {
+        if (n > end) list
+        else range(end - 1, end :: list)
+      }
+
+      range(m, Nil)
+    }
+  }
+
+  object P23 {
+    import P20._
+    def randomSelect[A](n: Int, list: List[A]): List[A] = {
+      def randomSelect(n: Int, list: List[A], result: List[A]): List[A] = {
+        if (n == 0) result
+        else {
+          val (remain, item) = removeAt(new Random(System.currentTimeMillis()).nextInt(n), list)
+          randomSelect(n - 1, remain, item :: result)
+        }
+      }
+
+      randomSelect(n, list, Nil)
+    }
+  }
+
+  object P24 {
+    import P22._
+    import P23._
+
+    def lotto(n: Int, max: Int) = randomSelect(n, range(1, max))
+  }
+
+  object P25 {
+    import P23._
+    def randomPermute[A](items: List[A]) = randomSelect(items.length, items)
+  }
 
   object P26 {
     def extractAndSlice[A](n: Int, list: List[A]): (A, List[A]) = {
@@ -263,7 +327,6 @@ object Problems {
 
   case class MTree[+T](value: T, children: List[MTree[T]]) {
     def this(value: T) = this(value, List())
-    //override def toString = "M(" + value.toString + " {" + children.map(_.toString).mkString(",") + "})"
     override def toString: String = value.toString + children.map(_.toString).mkString("") + "^"
   }
 
--- src/test/scala/TestCase.scala
+++ src/test/scala/TestCase.scala
@@ -3,104 +3,172 @@ import org.scalatest.FlatSpec
 import co.gyeongmin.problem.Problems._
 
 class TestCase extends FlatSpec {
-  import P01._
-  "P01" should "be 8" in assert(last(List(1, 1, 2, 3, 5, 8)) == 8)
+  "P01" should "be 8" in {
+    import P01._
+    assert(last(List(1, 1, 2, 3, 5, 8)) == 8)
+  }
 
-  import P02._
-  "P02" should "be 5" in assert(penultimate(List(1, 1, 2, 3, 5, 8)) == 5)
+  "P02" should "be 5" in {
+    import P02._
+    assert(penultimate(List(1, 1, 2, 3, 5, 8)) == 5)
+  }
 
-  import P03._
-  "P03" should "be 2" in assert(nth(2, List(1, 1, 2, 3, 4, 8)) == 2)
+  "P03" should "be 2" in {
+    import P03._
+    assert(nth(2, List(1, 1, 2, 3, 4, 8)) == 2)
+  }
 
-  import P04._
-  "P04" should "be 6" in assert(length(List(1, 1, 2, 3, 4, 8)) == 6)
+  "P04" should "be 6" in {
+    import P04._
+    assert(length(List(1, 1, 2, 3, 4, 8)) == 6)
+  }
 
-  "P05" should "be reverse" in assert(
-    (reverse(List(1, 1, 2, 3, 5, 8)) zip List(8, 5, 3, 2, 1, 1))
-      .forall(p => p._1 == p._2))
+  "P05" should "be reverse" in {
+    import P05._
+    assert(
+      reverse(List(1, 1, 2, 3, 5, 8)) == List(8, 5, 3, 2, 1, 1)
+    )
+  }
 
-  "P06" should "be true" in assert(isPalindrome(List(1, 2, 3, 2, 1)))
+  "P06" should "be true" in {
+    import P06._
+    assert(isPalindrome(List(1, 2, 3, 2, 1)))
+  }
 
-  "P07" should "flatten list" in assert(
-    flatten(List(List(1, 1), 2, List(3, List(5, 8)))) == List(1, 1, 2, 3, 5, 8)
-  )
+  "P07" should "flatten list" in {
+    import P07._
+    assert(
+      flatten(List(List(1, 1), 2, List(3, List(5, 8)))) == List(1, 1, 2, 3, 5, 8)
+    )
+  }
 
-  "P08" should "compress" in assert(
-    compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) == List('a, 'b, 'c, 'a, 'd, 'e))
+  "P08" should "compress" in {
+    import P08._
+    assert(
+      compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+        == List('a, 'b, 'c, 'a, 'd, 'e))
+  }
 
-  "P09" should "packing" in assert(
-    pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
-    == List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
-  )
+  "P09" should "packing" in {
+    import P09._
+    assert(
+      pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+        == List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+    )
+  }
 
-  "P10" should "encode things" in assert(
-    encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
-    == List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
-  )
+  "P10" should "encode things" in {
+    import P10._
+    assert(
+      encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+        == List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
+    )
+  }
 
-  "P11" should "equal to " in assert(
-    encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
-    == List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e))
-  )
+  "P11" should "equal to " in {
+    import P11._
+    assert(
+      encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+        == List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e))
+    )
+  }
 
-  "P12" should "decode things" in assert (
-    decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
-    == List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
-  )
+  "P12" should "decode things" in {
+    import P12._
+    assert (
+      decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+        == List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    )
+  }
 
-  "P13" should "encode direct" in assert (
-    encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
-      == List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
-  )
+  "P13" should "encode direct" in {
+    import P13._
+    assert (
+      encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+        == List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
+    )
+  }
 
-  "P14" should "duplicate everything" in assert (
-    duplicate(List('a, 'b, 'c, 'c, 'd))
-      == List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)
-  )
+  "P14" should "duplicate everything" in {
+    import P14._
 
-  "P15" should "duplicate n times everything" in assert (
-    duplicateN(3, List('a, 'b, 'c, 'c, 'd))
-      == List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd)
-  )
+    assert (
+      duplicate(List('a, 'b, 'c, 'c, 'd))
+        == List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)
+    )
+  }
 
-  "P16" should "drop every nth element" in assert (
-    drop(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
-      == List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k)
-  )
+  "P15" should "duplicate n times everything" in {
+    import P15._
 
-  "P17" should "split into two parts" in assert (
-    split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
-      == (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
-  )
+    assert (
+      duplicateN(3, List('a, 'b, 'c, 'c, 'd))
+        == List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd)
+    )
+  }
 
-  "P18" should "extract slice" in assert (
-    slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
-      == List('d, 'e, 'f, 'g)
-  )
+  "P16" should "drop every nth element" in {
+    import P16._
 
-  "P19" should "rotate list" in assert(
-    rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
-      == List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c)
-      &&
-    rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
-      == List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i)
-  )
+    assert (
+      drop(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+        == List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k)
+    )
+  }
 
-  "P20" should "remove kth element" in assert(
-    removeAt(1, List('a, 'b, 'c, 'd))
-      == (List('a, 'c, 'd),'b)
-  )
+  "P17" should "split into two parts" in {
+    import P17._
 
-  "P21" should "insert At" in assert(
-    insertAt('new, 1, List('a, 'b, 'c, 'd))
-      == List('a, 'new, 'b, 'c, 'd)
-  )
+    assert (
+      split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+        == (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+    )
+  }
 
-  "P22" should "range" in assert(
-    range(4, 9) == List(4, 5, 6, 7, 8, 9)
-  )
+  "P18" should "extract slice" in {
+    import P18._
+    assert (
+      slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+        == List('d, 'e, 'f, 'g)
+    )
+  }
+
+  "P19" should "rotate list" in {
+    import P19._
+    assert(
+      rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+        == List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c)
+        &&
+        rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+          == List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i)
+    )
+  }
+
+  "P20" should "remove kth element" in {
+    import P20._
+    assert(
+      removeAt(1, List('a, 'b, 'c, 'd))
+        == (List('a, 'c, 'd),'b)
+    )
+  }
+
+  "P21" should "insert At" in {
+    import P21._
+    assert(
+      insertAt('new, 1, List('a, 'b, 'c, 'd))
+        == List('a, 'new, 'b, 'c, 'd)
+    )
+  }
+
+  "P22" should "range" in {
+    import P22._
+    assert(
+      range(4, 9) == List(4, 5, 6, 7, 8, 9)
+    )
+  }
 
   "P23" should "random select" in {
+    import P23._
     println(randomSelect(3, List('a, 'b, 'c, 'd, 'f, 'g, 'h)))
     println(randomSelect(3, List('a, 'b, 'c, 'd, 'f, 'g, 'h)))
     println(randomSelect(3, List('a, 'b, 'c, 'd, 'f, 'g, 'h)))
@@ -110,12 +178,14 @@ class TestCase extends FlatSpec {
   }
 
   "P24" should "show lotto well" in {
+    import P24._
     assert(lotto(6, 49).forall(x => x >= 1 && x <= 49))
     assert(lotto(6, 49).length == 6)
     assert(lotto(6, 49).distinct.sorted == lotto(6, 49).sorted)
   }
 
   "P25" should "random permute" in {
+    import P25._
     val testInput = List('a, 'b, 'c, 'd, 'e, 'f)
     val rp = randomPermute(testInput)
     println(rp)
